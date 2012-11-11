d619ee143df4ab353e86e82247fbe0eebeae6653
Zachary Bornheimer <zbornheimer@wh-520-5-25.rc.usf.edu>
2012-11-11 16:33:30 -0500

    nlp.pl now reads files and process frequencies of words and surrounding words.  Check research-notes and README.md for details.

--- README.md
+++ README.md
@@ -2,3 +2,18 @@ artificial-intelligence-engine
 ==============================
 
 All my research for Artificial Intelligence
+
+
+Natural Language Processing: nlp.pl
+===================================
+Coded in: Perl 6
+Flow:
+* Use statistical analysis to discerne the most frequent morphemes in a given scenario [Extraction]
+* Use statistical analysis to discerne the context in which those morphemes appear [Extrapolation]
+* From those statistics, develop a primative set of rules that outline the morphemes of English.
+* From the morphemes, test adding the morphemes to words that appear in thoes scenarios and see if they are spelt correctly.  Learn from that information to determine interesting morpheme rules (like when to use {-es} vs {-s}).
+* From that information, be able to give the program words to represent classes of words (ie Verbs, Adjectives, Nouns, etc.)
+* Be able to get the program to print out a summarizing report of parts of speech given an input sentence AND be able to have the program change the morphemes in the words (like adding an {-s} or {-es} and similar).
+
+
+Eventually the nlp.pl program can be used to solve the Summarization Problem (given text input, summarize it).  Note, the problem with this program thus far is that the computer does not understand the meaning of words.  At the core, words can be defined in terms of other words, but at the very base level there is sensory association.  Without that sensory association, it cannot define any words.  So, after vision and robotics are complete, this program will be complete.
--- nlp.pl
+++ nlp.pl
@@ -61,19 +61,19 @@ multi sub learn(@filesToParse) {
         my $acqWordsCounter = 0;
         say "\nReading $_";
         print "\tParsing $_ ...";
-        my Str @text = lines ($_).IO;
+        my Str $text = slurp $_;
+        $text ~~ s:g/\n/\ /;
+        my Int $numWords = $text.split(' ').elems - 1; # index 0 
         say "done";
-        print "\tProcessing and storing words ...\r";
-        for 0 .. (@text.elems - 1) -> $index {
-            my $text = @text[$index];
-            my @uniqueWords;
-            print "\tProcessing the text ..." ~ ($index / @text.elems) * 100 ~ "%                        \r";
-            my @acqWords = $text.split(' '); # acquired words
-                my $acqWordsIndex = 0;
-            for (@acqWords) {
-                $_ ~~ s:g/<english::punctuation>/ /; # Needs to replace the punctuation with a space 
+        my @uniqueWords;
+        print "\tProcessing the text ...\r";
+        my @acqWords = $text.split(' '); # acquired words
+        my $acqWordsIndex = 0;
+        for (@acqWords) {
+            print "\tProcessing the text ..." ~ ($acqWordsIndex / $numWords) * 100 ~ "%                 \r";
+            $_ ~~ s:g/<english::punctuation>/ /; # Needs to replace the punctuation with a space 
 # because it uses a space as a delimiter
-                $_ ~~ s:g/\ //;
+            $_ ~~ s:g/\ //;
 
 ###################################################################################
 # The ending data structure for the following code will look something like this:
@@ -87,80 +87,78 @@ multi sub learn(@filesToParse) {
 # Note: frequency for surrounding words only happens after running the stats.
 ###################################################################################
 
-                if ($acqWordsIndex > 0) {
-                    my @wordsBefore;
-                    my @wordsAfter;
+            if ($acqWordsIndex > 0) {
+                my @wordsBefore;
+                my @wordsAfter;
 
 # Because we have not yet incremented $acqWordsIndex, it will automatically be 1 less than the current word
-                    my $startingNum = $acqWordsIndex - PreviousWords;
-                    if $startingNum < 0 {
-                        $startingNum = 0;
-                    }
-                    for $startingNum .. $acqWordsIndex - 1 {
-                        my $i = ($acqWordsIndex - 1) - $_;
-                        push @wordsBefore[$i], @acqWords[$_] if @acqWords[$_] ne "";
-                    }
+                my $startingNum = $acqWordsIndex - PreviousWords;
+                if $startingNum < 0 {
+                    $startingNum = 0;
+                }
+                for $startingNum .. $acqWordsIndex - 1 {
+                    my $i = ($acqWordsIndex - 1) - $_;
+                    push @wordsBefore[$i], @acqWords[$_] if @acqWords[$_] ne "";
+                }
 
 # Because we have not yet incremented $acqWordsIndex, it is 1 lower than the current value, so the current word is at $acqWordsIndex+1
-                    for $acqWordsIndex + 2 .. $acqWordsIndex + (SubsequentWords + 1) {
-                        my $i = ($acqWordsIndex + (SubsequentWords + 1)) - $_;
-                        if defined @acqWords[$_] {
-                            push @wordsAfter[$i], @acqWords[$_] if @acqWords[$_] ne "";
-                        }
-                    }
-
-                    for 0 .. (PreviousWords - 1) -> $val {
-                        my $t = @wordsBefore[$val][0];
-                        my @arrayBefore;
-                        if (defined $t && $t) {
-                            push @arrayBefore, @wordsBefore[$val][0];
-                        }
-                        push %wordsOnEitherSide{$_}[0][$val], @arrayBefore;
-                    }
-                    for 0 .. (SubsequentWords - 1) -> $val {
-                        my $y = @wordsAfter[$val][0];
-                        my @arrayAfter;
-                        if (defined $y && $y) {
-                            push @arrayAfter, @wordsAfter[$val][0];
-                        }
-                        push %wordsOnEitherSide{$_}[1][$val], @arrayAfter;
+                for $acqWordsIndex + 2 .. $acqWordsIndex + (SubsequentWords + 1) {
+                    my $i = ($acqWordsIndex + (SubsequentWords + 1)) - $_;
+                    if defined @acqWords[$_] {
+                        push @wordsAfter[$i], @acqWords[$_] if @acqWords[$_] ne "";
                     }
                 }
-                $acqWordsIndex++;
-            }
 
-            @acqWords = removeEmpty(@acqWords);
-            @uniqueWords = unique(@acqWords);
-            push @words, @uniqueWords;
-            $acqWordsCounter += @uniqueWords.elems;
+                for 0 .. (PreviousWords - 1) -> $val {
+                    my $t = @wordsBefore[$val][0];
+                    my @arrayBefore;
+                    if (defined $t && $t) {
+                        push @arrayBefore, @wordsBefore[$val][0];
+                    }
+                    push %wordsOnEitherSide{$_}[0][$val], @arrayBefore;
+                }
+                for 0 .. (SubsequentWords - 1) -> $val {
+                    my $y = @wordsAfter[$val][0];
+                    my @arrayAfter;
+                    if (defined $y && $y) {
+                        push @arrayAfter, @wordsAfter[$val][0];
+                    }
+                    push %wordsOnEitherSide{$_}[1][$val], @arrayAfter;
+                }
+            }
+            $acqWordsIndex++;
         }
+
+        #@acqWords = removeEmpty(@acqWords);
+        push @words, @acqWords ==> map { if $_ ne "" { $_ } };
         print "\tProcessing the text ...100%                            \r";
         say "\tProcessing the text ...done";
         say "Done Reading $_";
-        say "Learned " ~ $acqWordsCounter ~ " words\n";
+        say "Learned " ~ @acqWords.elems ~ " words\n";
+        say "Done reading the text in $nlpLiteratureDir.";
+        print "Creating the dictionary database by removing duplicate words in the word database ...";
+        @dictionary = unique(@words);
+        say "done";
     }
-    say "Done reading the text in $nlpLiteratureDir.";
-    print "Creating the dictionary database by removing duplicate words in the word database ...";
-    @dictionary = unique(@words);
-    say "done";
 }
 sub removeEmpty(@array) {
     my @a;
-    for (@array) {
-        push @a, $_ if $_ ne "";
+    for (0 .. @array.elems - 1) {
+        push @a, @array[$_] if @array[$_] ne "";
     }
     return @a;
 }
 
 
-sub stats($kind is rw, $num?) {
+sub stats($kind is rw, $num? = 0) {
     if ($kind eq "wordFrequency") {
         return wFrequency(@words, $num);
     } elsif ($kind eq "surroundingWordFrequency") {
-        # We do this for each one
+# We do this for each one
         for (%wordsOnEitherSide.keys) {
-            # Two Elements, 0 and 1
-            # We must get the arrays and replace them with hashes.
+# Two Elements, 0 and 1
+# We must get the arrays and replace them with hashes.
+# @alpha is a throwaway reference in both the blocks below.
             for %wordsOnEitherSide{$_}[0] -> @item is rw {
                 for 0 .. @item.elems-1 -> $index {
                     for @item[$index] -> @alpha is rw {
@@ -176,13 +174,11 @@ sub stats($kind is rw, $num?) {
                 }
             }
         }
-        say %wordsOnEitherSide;
     }
 }
 
-sub wFrequency(@array, $num) {
+sub wFrequency(@array, Int $num? = 0) {
     my %uniqueArrayHash;
-    my %freqHash;
     for (@array) {
         if defined %uniqueArrayHash{$_} {
             %uniqueArrayHash{$_} += 1;
@@ -193,15 +189,12 @@ sub wFrequency(@array, $num) {
 
     %uniqueArrayHash = reverse (%uniqueArrayHash.pairs.sort: { $^a.value <=> $^b.value });
     for (0 .. $num) {
-        %freqHash{(keys %uniqueArrayHash)[$_]} = %uniqueArrayHash{(keys %uniqueArrayHash)[$_]};
-    }
-    for (0 .. $num) {
         if (defined %uniqueArrayHash.keys[$_]) {
             my $key = %uniqueArrayHash.keys[$_];
             say $key ~ " => " ~ %uniqueArrayHash{$key} ~ " occurances.";
         }
     }
-    return %freqHash;
+    return %uniqueArrayHash;
 }
 
 sub sFrequency(@array is rw, $num?) {
@@ -237,14 +230,18 @@ sub sFrequency(@array is rw, $num?) {
     return %freqHash;
 }
 
-sub process($type, $num? = 42) {
+sub process($type, $num? = -1) {
     if ($type eq "word_frequency") {
-        if !$num {
+        if $num == -1 {
             my $num = prompt("How many words would you like to see? ");
         }
+        print "Processing Word Frequency ...";
         %frequencyHash = stats("wordFrequency", $num);
+        say "done.";
     } elsif ($type eq "surrounding_word_frequency") {
+        print "Processing Surrounding Word Frequency ...";
         stats("surroundingWordFrequency");
+        say "done";
     }
 }
 
@@ -283,32 +280,63 @@ sub recursiveDir($path, @array is rw) {
 }
 
 
-
-sub store($defaults? = 1) {
-    my $fileName = "";
+multi sub store(Str $f) {
+    store(0, $f);
+}
+multi sub store(Int $defaults? = 0, Str $fileName? = "") {
+    my $filename = $fileName;
     if !$defaults {
-        $fileName = prompt("What is the filename where I should save this data? -- Note, this WILL overwrite the data  [$restoreFile]  ");
+        if $filename eq "" {
+            $filename = prompt("What is the filename where I should save this data? -- Note, this WILL overwrite the data  [$restoreFile]  ");
+        }
     }
-    if chomp $fileName eq "" {
-        $fileName = $restoreFile;
+    if $filename eq "" {
+        say $filename; say $fileName;
+        $filename = $restoreFile;
     }
     print "Storing data ...";
-    my $fh = open $fileName, :w;
-    $fh.print('@letters = ' ~ @letters.perl ~ ';'); 
-    $fh.print('@words = ' ~ @words.perl ~ ';'); 
-    $fh.print('@dictionary = ' ~ @dictionary.perl ~ ';'); 
-    $fh.print('%wordsOnEitherSide = ' ~ %wordsOnEitherSide.perl ~ ';'); 
-    $fh.print('%frequencyHash = ' ~ %frequencyHash.perl ~ ';'); 
-    $fh.print('@letters = ' ~ @letters.perl ~ ';'); 
+    my $fh = open $filename, :w;
+    for (@letters) {
+        if $_ ne "" && $_ ~~ s:g/\"// {
+           $fh.print('@letters.push("' ~ $_ ~ '");');    
+        }
+    }
+    for (@words) {
+        if $_ ne "" && $_ ~~ s:g/\"// {
+           $fh.print('@words.push("' ~ $_ ~ '");');    
+        }
+    }
+    for (@dictionary) {
+        if $_ ne "" && $_ ~~ s:g/\"// {
+           $fh.print('@dictionary.push("' ~ $_ ~ '");');    
+        }
+    }
+
+    for 0 .. 1 -> $i {
+        for %wordsOnEitherSide[$i].keys -> $key is rw {
+            #$key ~~ s:g/\"//;
+            if $key && $key ne "" {
+                for 0 .. 1 -> $j {
+                    for %wordsOnEitherSide[$i]{$key}[$j] -> $u {
+                        $fh.print( '%wordsOnEitherSide[' ~ $i ~ ']{'~ $key.perl ~'}[' ~ $j ~ '].push(' ~ $u.perl ~ ');' );
+                    }
+                }
+            }
+        }
+    }
+
+    for %frequencyHash.keys -> $key {
+        $fh.print('%frequencyHash{' ~ $key.perl ~ '}=' ~ %frequencyHash{$key}.perl ~ ';'); 
+    }
     $fh.close();
 
     say "done."
 }
 
-multi sub load(Int $defaults? = 1, Str $fileName? = "") {
+multi sub load(Int $defaults? = 0, Str $fileName? = "") {
     my $filename = $fileName;
-    if $filename eq "" {
-        if $defaults {
+    if !$defaults {
+        if $filename eq "" {
             $filename = prompt("What is the filename which should be loaded?\nNote, this will overwrite any data that has been learned this session.  Make sure this is done first. [$restoreFile]  ");
         }
     }
@@ -333,7 +361,7 @@ sub repl() {
     while (1) {
         $input = prompt(" >> ");
         my $parserResults = replGrammar.parse($input);
-        # Substitutions
+# Substitutions
         if defined $parserResults<command><command><expression> && $parserResults<command><command><expression> ne "" {
             my $exp = $parserResults<command><command><expression>;
             my $origExp = $exp;
@@ -345,7 +373,7 @@ sub repl() {
             } else {
                 $code = "defined(&$exp)";
             }
-            if ($exp ~~ /\./ || $code && !eval $code) {
+            if ($exp ~~ m/\./ || $code && !eval $code) {
                 my $sub = '"' ~ $exp ~ '"';
                 $input ~~ s/$exp/$sub/;
             }
@@ -363,11 +391,12 @@ sub repl() {
                 $keyword = $parserResults<command><command><sym>;
             }
         }
-        if (defined ($keyword)) {
+        if defined ($keyword) {
             if $keyword eq "parse" {
                 $input ~~ s/($keyword\s+)(.*)/{$0}"{$1}"/;
+                $input ~~ s:g/\"\"/"/;
             }
-            if (eval('defined(&' ~ $keyword ~ ')')) {
+            if  eval('defined(&' ~ $keyword ~ ')') {
                 eval $input;
                 say "";
             }
@@ -378,6 +407,5 @@ sub repl() {
         }
     }
 }
-
 
 repl();
