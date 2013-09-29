db5f54032000500120a25688e2bfa51fbe5ada29
Z. Bornheimer <z@chary.me>
2013-09-29 18:25:43 -0400

    Fixed bugs in file.h and tested - it works now...

--- c/file.h
+++ c/file.h
@@ -11,36 +11,40 @@
 typedef char* file;
 
 /* prototypes, for good measure */
-void read_by_char(const int c, file *buffer, int *charCount);
+void read_by_char(const int c, file *buffer, int *charCount, int *bytes);
 file read_file(const char *filename);
 file ask_for_file();
 /* end prototypes */
 
 /* adds a character to a file (char *array) type */
-void read_by_char(const int c, file *buffer, int *charCount)
+void read_by_char(const int c, file *buffer, int *charCount, int *bytes)
 {
-    (*buffer)[++(*charCount)] = '\0';
-    *buffer = realloc(*buffer, sizeof(char) * *charCount);
-    if(buffer == NULL) {
-        printf("Cannot allocate memory for reading file!\n");
-        exit(1);
+    if (*bytes <= *charCount) {
+        *bytes = *bytes; 
+        *buffer = realloc(*buffer, sizeof(char) * *bytes);
+        if(buffer == NULL) {
+            --(*charCount);
+            printf("Cannot reallocate memory for reading file!\n");
+            exit(1);
+        }
     }
-    (*buffer)[*charCount] = c;
+    (*buffer)[(*charCount)++] = c;
+    (*buffer)[(*charCount)] = '\0';
 }
 
 /* returns a file's contents in: file fileContents (char *fileContents) */
 file read_file(const char *filename)
 {
-    int c, charCount = -1;
+    int c, charCount = 0, bytes = 1;
     FILE *alpha;
 
-    file f = malloc(sizeof(char));
+    file f = malloc(sizeof(char) * bytes);
 
-    if((alpha = fopen(f, "r")) == NULL)
-        printf("Couldn't read file: %s\n", f);
+    if((alpha = fopen(filename, "r")) == NULL)
+        printf("Couldn't read file: %s", filename);
 
     while((c = fgetc(alpha)) != EOF)
-        read_by_char(c, &f, &charCount);
+        read_by_char(c, &f, &charCount, &bytes);
 
     fclose(alpha);
     return f;
@@ -49,13 +53,13 @@ file read_file(const char *filename)
 /* user prompt for filename */
 file ask_for_file()
 {
-    int c, charCount = -1;
+    int c, charCount = 0, bytes = 1;
     char *filename;
 
-    filename  = malloc(sizeof(char));
+    filename  = malloc(sizeof(char) * bytes);
 
     while ((c = getchar()) != EOF && c != '\n')
-        read_by_char(c, &filename, &charCount);
+        read_by_char(c, &filename, &charCount, &bytes);
 
     return filename;
 }
