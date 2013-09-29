a55dc42ae04f1c7d756cdabc4637e859d830f771
Z. Bornheimer <z@chary.me>
2013-09-29 18:06:52 -0400

    Fixed lack of '\0' terminator in read_by_char

--- c/file.h
+++ c/file.h
@@ -11,38 +11,36 @@
 typedef char* file;
 
 /* prototypes, for good measure */
-void read_by_char(const int c, file *buffer, int *charCount, int *bytes);
+void read_by_char(const int c, file *buffer, int *charCount);
 file read_file(const char *filename);
 file ask_for_file();
 /* end prototypes */
 
 /* adds a character to a file (char *array) type */
-void read_by_char(const int c, file *buffer, int *charCount, int *bytes)
+void read_by_char(const int c, file *buffer, int *charCount)
 {
-    if (*bytes <= *charCount) {
-        ++(*bytes); 
-        *buffer = realloc(*buffer, sizeof(char) * *bytes);
-        if(buffer == NULL) {
-            printf("Cannot reallocate memory for reading file!\n");
-            exit(1);
-        }
+    (*buffer)[++(*charCount)] = '\0';
+    *buffer = realloc(*buffer, sizeof(char) * *charCount);
+    if(buffer == NULL) {
+        printf("Cannot allocate memory for reading file!\n");
+        exit(1);
     }
-    (*buffer)[(*charCount)++] = c;
+    (*buffer)[*charCount] = c;
 }
 
 /* returns a file's contents in: file fileContents (char *fileContents) */
 file read_file(const char *filename)
 {
-    int c, charCount = 0, bytes = 1;
+    int c, charCount = -1;
     FILE *alpha;
 
-    file f = malloc(sizeof(char) * bytes);
+    file f = malloc(sizeof(char));
 
     if((alpha = fopen(f, "r")) == NULL)
         printf("Couldn't read file: %s\n", f);
 
     while((c = fgetc(alpha)) != EOF)
-        read_by_char(c, &f, &charCount, &bytes);
+        read_by_char(c, &f, &charCount);
 
     fclose(alpha);
     return f;
@@ -51,13 +49,13 @@ file read_file(const char *filename)
 /* user prompt for filename */
 file ask_for_file()
 {
-    int c, charCount = 0, bytes = 1;
+    int c, charCount = -1;
     char *filename;
 
-    filename  = malloc(sizeof(char) * bytes);
+    filename  = malloc(sizeof(char));
 
     while ((c = getchar()) != EOF && c != '\n')
-        read_by_char(c, &filename, &charCount, &bytes);
+        read_by_char(c, &filename, &charCount);
 
     return filename;
 }
