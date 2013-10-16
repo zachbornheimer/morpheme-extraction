d742ba1b15d9e93c763b3aecde828880769976af
Z. Bornheimer <z@chary.me>
2013-10-16 00:12:52 -0400

    Updated file.h for bugs and stupidity
    It was a very stupid thing to try and intentionally mask
    the char* with file as it will eventually lead to someone
    forgetting to free(f) on a read file.  Also, I reduced some
    error checking compelxity and eliminated an off by one byte
    error in read_by_char that only manifested when running on the
    actual corpus.

--- file.h
+++ file.h
@@ -4,27 +4,26 @@
  * Ex: 
  * main()
  * {
- *     file f = read_file(ask_for_file());
+ *     char *f = read_file(ask_for_file());
  *     printf("%s\n", f);
  * }
  */
  
 #include <stdlib.h>
 #include <stdio.h>
-
-typedef char* file;
+#include <errno.h>
 
 /* prototypes, for good measure */
-void read_by_char(const int c, file *buffer, int *charCount, int *bytes);
-file read_file(const char *filename);
-file ask_for_file();
+void read_by_char(const int c, char **buffer, int *charCount, int *bytes);
+char *read_file(const char *filename);
+char *ask_for_file();
 /* end prototypes */
 
 /* adds a character to a file (char *array) type */
-void read_by_char(const int c, file *buffer, int *charCount, int *bytes)
+void read_by_char(const int c, char **buffer, int *charCount, int *bytes)
 {
     if (*bytes <= *charCount) {
-        *bytes = *charCount + 1; 
+        *bytes = *charCount + 2; 
         *buffer = realloc(*buffer, sizeof(char) * *bytes);
         if(buffer == NULL) {
             --(*charCount);
@@ -36,32 +35,34 @@ void read_by_char(const int c, file *buffer, int *charCount, int *bytes)
     (*buffer)[(*charCount)] = '\0';
 }
 
-/* returns a file's contents in: file fileContents (char *fileContents) */
-file read_file(const char *filename)
+/* returns a file's contents in: char *fileContents */
+char* read_file(const char *filename)
 {
     int c, charCount = 0, bytes = 1;
     FILE *alpha;
 
-    file f = malloc(sizeof(char) * bytes);
+    char *f = malloc(sizeof(char) * bytes);
 
-    if((alpha = fopen(filename, "r")) == NULL)
-        printf("Couldn't read file: %s", filename);
-
-    while((c = fgetc(alpha)) != EOF)
-        read_by_char(c, &f, &charCount, &bytes);
+    if ((alpha = fopen(filename, "r")) == NULL) {
+        printf("Couldn't read file: %s Error: %s\n", filename, strerror(errno));
+        fclose(alpha);
+        return NULL;
+    } else {
+        while((c = fgetc(alpha)) != EOF)
+            read_by_char(c, &f, &charCount, &bytes);
+    }
 
     fclose(alpha);
-
     return f;
 }
 
 /* user prompt for filename */
-file ask_for_file()
+char *ask_for_file()
 {
     int c, charCount = 0, bytes = 1;
     char *filename;
 
-    filename  = malloc(sizeof(char) * bytes);
+    filename = malloc(sizeof(char) * bytes);
 
     while ((c = getchar()) != EOF && c != '\n')
         read_by_char(c, &filename, &charCount, &bytes);
